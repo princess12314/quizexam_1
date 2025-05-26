c7b427632313a9a111a8f450850f694eadbeb858
princess12314 <princessbatacpanaginip@gmail.com>
2025-05-26 14:39:35 +0800

    these are all the changes

--- app/Http/Controllers/employeecontroller.php
+++ app/Http/Controllers/employeecontroller.php
@@ -23,12 +23,12 @@ class employeecontroller extends Controller
     public function store(Request $request)
     {
     $request->validate([
-        'fname' => 'required|max:255|',
-        'lname' => 'required|max:255|',
-        'midname' => 'required|max:255|',
-        'age' => 'required|',
-        'address' => 'required|max:255|',
-        'zip' => 'required|',
+        'fname' => 'required|max:255|string',
+        'lname' => 'required|max:255|striing',
+        'midname' => 'required|max:255|string',
+        'age' => 'required|integer',
+        'address' => 'required|max:255|string',
+        'zip' => 'required|integer',
         
     ]);
 
